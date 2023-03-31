7f035008d30f32e6bc2c60a55f88bd51a073ac0f
Morganfp <morganpurcellp@gmail.com>
2023-03-31 08:55:54 -0500

    Initial Settings

--- CC/settings.py
+++ CC/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
