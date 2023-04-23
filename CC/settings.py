e9ef3804d8904f4370517e90fe544b144427388c
Floris Kruger <florisjkruger4@gmail.com>
2023-04-22 19:24:32 -0500

    added Requirements.txt and modified settings.py

--- CC/settings.py
+++ CC/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = 'django-insecure-$u6y%f$z!2+3mz4gu-1olkw1)cag^sy418ob#2g70)b&s+vye%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cc-production-832e.up.railway.app', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['https://cc-production-832e.up.railway.app/','https://127.0.0.1']
 
 # Application definition
 
